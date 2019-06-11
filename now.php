3044b1f678a1aa72cbe33bdc03ee37c4b7a22942
uwem udoh <slimudoh@yahoo.com>
2019-06-11 18:06:09 +0100

    new files

---
+++ now.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Becker Construction Company">
+    <meta name="keywords" content="Construction">
+    <meta name="author" content="Uwem Slim Udoh">    
+    
+    <title>Now selling/leasing :: Becker Construction Company</title>
+
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="images/favicon/favicon.png">
+
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/modal.css">
+    
+    <!-- font awesome -->
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="drop-menu" id="dropmenu">
+      <ul>
+        <a href="index.php"><li>HOME</li></a></a>
+        <a href="service.php"><li>SERVICES</li></a>
+        <a href="project.php"><li>PROJECTS</li></a>
+        <a href="now.php"><li>NOW SELLING/LEASING</li></a>
+        <a href="contct.php"><li>CONTACT</li></a>              
+      </ul>    
+
+      <div style="width: 100%; padding: 10px 0;">
+        <div class="menu-close" onclick="closeMenu()">
+          <img src="images/icon/up.png">          
+        </div>
+      </div>  
+    </div>
+
+    <div class="bg">
+      <ul class="rslides">
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/1.png);"></div>
+        </li>
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/2.png);"></div>
+        </li>
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/3.png);"></div>
+        </li>
+      </ul>
+
+      <div class="container-box">
+
+        <div class="header">
+          <div class="container desktop" id="desktop">
+            <div class="row">
+              <div class="col-md-2" style="padding:0;">
+                <a href="index.php"><img src="images/logo/logo.png"></a>
+              </div>
+              <div class="col-md-2"></div>
+              <div class="col-md-8" style="padding:0;">
+                <div class="nav">
+                  <ul>
+                    <a href="index.php"><li>HOME</li></a>
+                    <a href="service.php"><li>SERVICES</li></a>
+                    <a href="project.php"><li>PROJECTS</li></a>
+                    <a href="now.php"><li  class="active">NOW SELLING/LEASING</li></a>
+                    <a href="contct.php"><li>CONTACT</li></a>              
+                  </ul>
+                </div>                
+              </div>          
+            </div>
+          </div>
+
+          <div class="mobile" id="mobile">
+            <div class="logo">
+              <a href="index.php"><img src="images/logo/logo.png"></a>
+            </div>
+            <div class="menu" >
+              <img src="images/icon/menu.png" onclick="openMenu()">
+            </div>            
+          </div>
+        </div>
+
+        <div style="margin-top: 50px;"></div>
+
+       <div class="container" style="background: rgba(0, 0, 0, .7);border-radius: 5px;overflow: hidden;">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="left-box">
+                <span style="font-size: 18px;font-weight: 600;">NOW</span><br>
+                <span id="cover" style="font-size: 50px;font-weight: 600;">
+                  SELLING/LEASING                
+                </span>       
+              </div>              
+            </div>
+            <div class="col-md-6">
+              <div class="right-box">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                                
+              </div>
+            </div>            
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+
+          <div class="row">
+            <div class="col-md-12 text-center" style="font-family: 'Montserrat', sans-serif;">
+              <h2>NOW LEASING</h2>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+
+          <div class="row">
+            <div class="col-md-12 text-center" style="font-family: 'Montserrat', sans-serif;">
+              <h2>NOW SELLING</h2>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+
+          <div class="row">
+            <div class="col-md-12 text-center" style="font-family: 'Montserrat', sans-serif;">
+              <h2>FEATURED PRODUCTS</h2>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+        </div> 
+      </div>      
+    </div>
+
+    <div class="footer">  
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3" style="font-size: 12px;">
+            <div class="copyright">
+              &copy2017. BECKER CONSTRUCTION COMPANY
+            </div>            
+          </div>
+          <div class="col-md-6"></div>
+          <div class="col-md-3">
+            <div class="social">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              </ul>     
+            </div>
+          </div>
+        </div>
+      </div>      
+          
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+    <script src="js/custom.js"></script>
+
+    <!-- slider -->
+    <script>
+      $(function() {
+        $(".rslides").responsiveSlides();
+      });
+    </script>
+
+    <script type="text/javascript">
+      
+      function openMenu() {
+        //document.getElementById("dropmenu").style.width = "100%";       
+
+        document.getElementById("dropmenu").style.WebkitAnimation = "moveout .5s";
+        document.getElementById("dropmenu").style.animation = "moveout .5s";
+        document.getElementById("dropmenu").style.width = "100%";        
+      }
+
+      function closeMenu() {
+        document.getElementById("dropmenu").style.WebkitAnimation = "movein .5s";
+        document.getElementById("dropmenu").style.animation = "movein .5s";
+        document.getElementById("dropmenu").style.width = "0%";
+      }
+
+    </script>
+
+    <script type="text/javascript">
+      onresize = onload = function() {
+        var desktopMenu = document.getElementById("desktop");
+        var mobileMenu = document.getElementById("mobile");
+        var cover = document.getElementById("cover");
+        var dropMenu = document.getElementById("dropmenu");
+
+
+        if (window.innerWidth < 1000 || document.documentElement.clientWidth < 1000 || document.body.clientWidth < 1000)  {
+          desktopMenu.style.display = "none";
+          mobileMenu.style.display = "block";
+        } else if (window.innerWidth >= 1000 || document.documentElement.clientWidth >= 1000 || document.body.clientWidth >= 1000) {
+          desktopMenu.style.display = "block";
+          mobileMenu.style.display = "none";
+        }
+
+        if (window.innerWidth < 1000 || document.documentElement.clientWidth < 1000 || document.body.clientWidth < 1000)  {
+          dropMenu.style.display = "initial";
+        } else if (window.innerWidth >= 1000 || document.documentElement.clientWidth >= 1000 || document.body.clientWidth >= 1000) {
+          dropMenu.style.display = "none";
+        }
+
+
+        if (window.innerWidth < 500 || document.documentElement.clientWidth < 500 || document.body.clientWidth < 500) {
+          cover.style.fontSize = "30px";
+        } else {
+          cover.style.fontSize = "50px";
+        } 
+      }   
+
+    </script>
+
+
+
+
+  </body>
+</html>
---
+++ service.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Becker Construction Company">
+    <meta name="keywords" content="Construction">
+    <meta name="author" content="Uwem Slim Udoh">    
+    
+    <title>Services :: Becker Construction Company</title>
+
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="images/favicon/favicon.png">
+
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/modal.css">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="drop-menu" id="dropmenu">
+      <ul>
+        <a href="index.php"><li>HOME</li></a></a>
+        <a href="service.php"><li>SERVICES</li></a>
+        <a href="project.php"><li>PROJECTS</li></a>
+        <a href="now.php"><li>NOW SELLING/LEASING</li></a>
+        <a href="contct.php"><li>CONTACT</li></a>              
+      </ul>    
+
+      <div style="width: 100%; padding: 10px 0;">
+        <div class="menu-close" onclick="closeMenu()">
+          <img src="images/icon/up.png">          
+        </div>
+      </div>  
+    </div>
+
+    <div class="bg">
+      <ul class="rslides">
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/1.png);"></div>
+        </li>
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/2.png);"></div>
+        </li>
+        <li>
+          <div class="slider-image" style="background-image: url(images/banner/3.png);"></div>
+        </li>
+      </ul>
+
+      <div class="container-box">
+
+        <div class="header">
+          <div class="container desktop" id="desktop">
+            <div class="row">
+              <div class="col-md-2" style="padding:0;">
+                <a href="index.php"><img src="images/logo/logo.png"></a>
+              </div>
+              <div class="col-md-2"></div>
+              <div class="col-md-8" style="padding:0;">
+                <div class="nav">
+                  <ul>
+                    <a href="index.php"><li>HOME</li></a>
+                    <a href="service.php"><li class="active">SERVICES</li></a>
+                    <a href="project.php"><li>PROJECTS</li></a>
+                    <a href="now.php"><li>NOW SELLING/LEASING</li></a>
+                    <a href="contct.php"><li>CONTACT</li></a>              
+                  </ul>
+                </div>                
+              </div>          
+            </div>
+          </div>
+
+          <div class="mobile" id="mobile">
+            <div class="logo">
+              <a href="index.php"><img src="images/logo/logo.png"></a>
+            </div>
+            <div class="menu" >
+              <img src="images/icon/menu.png" onclick="openMenu()">
+            </div>            
+          </div>
+        </div>
+
+        <div style="margin-top: 50px;"></div>
+
+       <div class="container" style="background: rgba(0, 0, 0, .7);border-radius: 5px;overflow: hidden;">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="left-box">
+                <span style="font-size: 18px;font-weight: 600;">OUR</span><br>
+                <span style="font-size: 50px;font-weight: 600;">
+                  SERVICES                
+                </span>       
+              </div>              
+            </div>
+            <div class="col-md-6">
+              <div class="right-box">
+                Becker Construction Company guarantees reliability, qualilty and professional services at reasonable prices.
+              </div>
+            </div>            
+          </div>
+
+          <div style="margin-top: 20px;"></div>
+
+
+          <div class="row">              
+
+            <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="service-list">
+                <h5>ARCHITECTURAL DESIGN</h5>
+                <p>
+                  Our focus is on the components and elements of a structure by unifying them into a coherent and functional whole, according to a particular approach in achieving the objective desire of the design that surpasses the expectation of the client.
+                </p> 
+              </div>
+            </div>   
+
+            <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="service-list">
+                <h5>RENOVATION OF EXISTING STRUCTURE</h5>
+                <p>
+                  we have the ability for transforming, remodelling and reviving dilapidated structures into a unique modern structure with an effective and functional creativity
+                </p> 
+              </div>
+            </div>   
+
+            <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="service-list">
+                <h5>INTERIOR DECORATION</h5>
+                <p>
+                  With our intellectual knowledge we are known for using client’s behavioural pattern in creating functional spaces within a building
+                </p> 
+              </div>
+            </div>   
+                          
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+
+          <div class="row">
+            <div class="col-md-12 text-center" style="font-family: 'Montserrat', sans-serif;">
+              <h2>FEATURED PRODUCTS</h2>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+              </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="product">
+                <img src="images/featured/1.png" alt="">                
+              </div>
+              <div class="project-list">
+                <h5>INTERIOR DESIGN IKOYI LOGOS</h5>
+              </div>
+              <div class="next">
+                  <a href="#"><img src="images/icon/next.png" alt=""></a>
+              </div>
+            </div>
+          </div>
+
+          <div style="margin-top: 50px;"></div>
+        </div> 
+      </div>      
+    </div>
+
+    <div class="footer">  
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3" style="font-size: 12px;">
+            <div class="copyright">
+              &copy2017. BECKER CONSTRUCTION COMPANY
+            </div>            
+          </div>
+          <div class="col-md-6"></div>
+          <div class="col-md-3">
+            <div class="social">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              </ul>     
+            </div>
+          </div>
+        </div>
+      </div>      
+          
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+    <script src="js/custom.js"></script>
+
+    <!-- slider -->
+    <script>
+      $(function() {
+        $(".rslides").responsiveSlides();
+      });
+    </script>
+
+    <script type="text/javascript">
+      
+      function openMenu() {
+        //document.getElementById("dropmenu").style.width = "100%";       
+
+        document.getElementById("dropmenu").style.WebkitAnimation = "moveout .5s";
+        document.getElementById("dropmenu").style.animation = "moveout .5s";
+        document.getElementById("dropmenu").style.width = "100%";        
+      }
+
+      function closeMenu() {
+        document.getElementById("dropmenu").style.WebkitAnimation = "movein .5s";
+        document.getElementById("dropmenu").style.animation = "movein .5s";
+        document.getElementById("dropmenu").style.width = "0%";
+      }
+
+    </script>
+
+    <script type="text/javascript">
+      onresize = onload = function() {
+        var desktopMenu = document.getElementById("desktop");
+        var mobileMenu = document.getElementById("mobile");
+        var cover = document.getElementById("cover");
+        var dropMenu = document.getElementById("dropmenu");
+
+
+        if (window.innerWidth < 1000 || document.documentElement.clientWidth < 1000 || document.body.clientWidth < 1000)  {
+          desktopMenu.style.display = "none";
+          mobileMenu.style.display = "block";
+        } else if (window.innerWidth >= 1000 || document.documentElement.clientWidth >= 1000 || document.body.clientWidth >= 1000) {
+          desktopMenu.style.display = "block";
+          mobileMenu.style.display = "none";
+        }
+
+        if (window.innerWidth < 1000 || document.documentElement.clientWidth < 1000 || document.body.clientWidth < 1000)  {
+          dropMenu.style.display = "initial";
+        } else if (window.innerWidth >= 1000 || document.documentElement.clientWidth >= 1000 || document.body.clientWidth >= 1000) {
+          dropMenu.style.display = "none";
+        }
+
+
+        if (window.innerWidth < 500 || document.documentElement.clientWidth < 500 || document.body.clientWidth < 500) {
+          cover.style.fontSize = "30px";
+        } else {
+          cover.style.fontSize = "50px";
+        } 
+      }   
+
+    </script>
+
+
+
+
+  </body>
+</html>
